07cefe2d86b651511991817fb39e947ce9ee33fd
Adella <adellakenn@gmail.com>
2019-06-25 11:34:41 +0000

    Automatically backed up by Learn

--- spec/bundler_spec.rb
+++ spec/bundler_spec.rb
@@ -1,4 +1,5 @@
 require 'bundler'
+require 'pry'
 
 describe "Bundler" do
   before :all do
@@ -11,6 +12,7 @@ describe "Bundler" do
   describe "Gemfile" do
     before :all do
       @gemfile_text = File.read('Gemfile')
+  # binding.pry
     end
 
     it "has correct syntax" do
